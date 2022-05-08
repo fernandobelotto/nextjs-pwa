eb7cffcac8c03ea95701ea26b20178bc7d05f8c1
Fernando Bosco <fbosco@dlocal.com>
2022-05-08 20:29:12 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://dlocal.com">dLocal PWA</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
